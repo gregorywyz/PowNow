7f3f197d2b6b2c26f01c36775b3aa319dba35544
gregorywyz <gregwyszynski@gmail.com>
2015-05-31 14:52:07 -0700

    working the search out of the homecontroller now

--- assets/js/app/controllers/HomeCtrl.js
+++ assets/js/app/controllers/HomeCtrl.js
@@ -1,15 +1,22 @@
-PowApp.controller('HomeCtrl',['$scope','$rootScope','$location',function($scope,$rootScope,$location){
+PowApp.controller('HomeCtrl',['$scope','$rootScope','$location','$http',function($scope,$rootScope,$location,$http){
 
   console.log("HomeCtrl initiated!");
 
+  // html5 gets geolocation of user
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $scope.currentLocation = position;
+    console.log('currentLocation', $scope.currentLocation);
+    console.log('currentLong', $scope.currentLocation.coords.longitude);
+    console.log('currentLat', $scope.currentLocation.coords.latitude);
+  });
+
   $scope.findResorts = function() {
 
-    // html5 gets geolocation of user
-    navigator.geolocation.getCurrentPosition(function(position) {
-      $scope.currentLocation = position;
-      console.log('currentLocation', $scope.currentLocation);
+    // returns array of all ski resorts
+    $http.get('/api/resort').success(function(data){
+      console.log('resorts:',data);
+      $scope.resorts = data;
     });
-    $location.path('/resort');
   };
 
 }]);
