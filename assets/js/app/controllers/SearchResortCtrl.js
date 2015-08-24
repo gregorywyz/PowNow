6804b404784c6609c0fbcf7c8e4ac46ed32c655a
gregorywyz <gregwyszynski@gmail.com>
2015-08-23 19:40:55 -0700

    added open weather api to return current weather for closest ski area results

--- assets/js/app/controllers/SearchResortCtrl.js
+++ assets/js/app/controllers/SearchResortCtrl.js
@@ -11,8 +11,36 @@ PowApp.controller('SearchResortCtrl',['$scope','$rootScope','$http','$location',
       longitude: query.lng
     }
   }).success(function(data){
-    console.log('resorts:',data);
+    // returns array of closest ski areas
+    // console.log('resorts:',data);
     $scope.resorts = data.resorts;
+
+    // make quick call to openweather to display current conditions
+    data.resorts.forEach(function(resort,idx) {
+
+      // build up url for newSnow
+      var req = {
+        url: "http://api.openweathermap.org/data/2.5/weather",
+        params: {
+          units: 'imperial',
+          lat: resort.location.lat,
+          lon: resort.location.long
+        }
+      };
+
+      // call openweather api for current conditions
+      $http(req).success(function(snowData) {
+        $scope.resorts[idx].desc = snowData.weather[0].main;
+        $scope.resorts[idx].temp = (Math.round(snowData.main.temp * 10) / 10) + ' \xB0F';
+        if (snowData.snow) {
+          $scope.resorts[idx].snow = (Math.round(snowData.snow['3h'] * 10) / 10) + ' in Fresh Snow';
+        } else {
+          $scope.resorts[idx].snow = 'No Freshies';
+        }
+      });
+
+    });
+    console.log("final scope",$scope.resorts);
   });
 
 }]);
