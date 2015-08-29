1fc565ae656458d3203deafb42876b172d8e6e33
gregorywyz <gregwyszynski@gmail.com>
2015-08-29 13:52:58 -0700

    created function to handle loader logic and added gif to html

--- assets/js/app/controllers/HomeCtrl.js
+++ assets/js/app/controllers/HomeCtrl.js
@@ -1,12 +1,46 @@
 PowApp.controller('HomeCtrl',['$scope','$rootScope','$http',function($scope,$rootScope,$http){
 
   $scope.currentLocation = false;
+  $scope.loading = false;
 
   // get GPS location on home page load
-  navigator.geolocation.getCurrentPosition(function(position) {
-    $scope.$evalAsync(function(){
-      $scope.currentLocation = position.coords;
-    });
-  });
+  // navigator.geolocation.getCurrentPosition(function(position) {
+  //   console.log('gathering location');
+  //   $scope.loading = true;
+  //   $scope.$evalAsync(function(){
+  //     console.log('location found');
+  //     $scope.loading = false;
+  //     $scope.currentLocation = position.coords;
+  //   });
+  // });
+
+  var geoLocateMe = function() {
+    // check if navigotor is available on browser
+    if (!navigator.geolocation){
+      alert('Geolocation is not supported by your browser. This app is just not for you.');
+      return;
+    };
+
+    // set current location and turns off loader
+    function succes(position) {
+      console.log('location found');
+      $scope.$evalAsync(function(){
+        $scope.loading = false;
+        $scope.currentLocation = position.coords;
+      });
+    };
+
+    function error() {
+      console.log('sorry, we could not find your BBQ');
+    };
+
+    // start loader
+    $scope.loading = true;
+
+    // find geolocation
+    navigator.geolocation.getCurrentPosition(succes, error);
+  };
+  geoLocateMe();
+
 
 }]);
