4d7ed573a1a6962c9c9a6eeadbb7ac4f2c49fc10
gregorywyz <gregwyszynski@gmail.com>
2015-08-29 23:40:49 -0700

    cleaning up code

--- api/controllers/ResortController.js
+++ api/controllers/ResortController.js
@@ -121,8 +121,8 @@ if(disableApi){
       // EXTERNAL API CALL FOR WEATHER RADAR
       var radar = 'http://api.wunderground.com/api/' + process.env.WU_KEY + '/animatedradar/image.gif?centerlat=' + resort[0].location.lat + '&centerlon=' + resort[0].location.long + '&radius=30&width=450&height=450&rainsnow=1&newmaps=1&timelabel=1&timelabel.y=10&num=15&delay=50&reproj.automerc=1';
 
-// NOTE: example from api docs
-// "http://api.wunderground.com/api/9ab9196cdf6818a6/radar/image.gif?maxlat=47.709&maxlon=-69.263&minlat=31.596&minlon=-97.388&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1"
+      // NOTE: example from api docs
+      // "http://api.wunderground.com/api/9ab9196cdf6818a6/radar/image.gif?maxlat=47.709&maxlon=-69.263&minlat=31.596&minlon=-97.388&width=640&height=480&rainsnow=1&timelabel=1&timelabel.x=525&timelabel.y=41&reproj.automerc=1"
 
       // get local radar for ski area
       // then pipe reponse to url (../radar.gif)
